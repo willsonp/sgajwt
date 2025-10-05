d2c0791faa93aef8badadd7a4acac030e9738321
willsonp <willvalentinpf@gmail.com>
2025-10-05 16:37:19 -0400

    Protecion de Rutas basado en Role

--- src/main/java/sga/sgajwt/Controllers/PrincipalController.java
+++ src/main/java/sga/sgajwt/Controllers/PrincipalController.java
@@ -1,5 +1,6 @@
 package sga.sgajwt.Controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,19 @@ public class PrincipalController {
 
     @GetMapping()
     public String welcomen() {
-    return "Si puedes Visualizar esto es porque tienes permisos Privado.";
-}
+    return "Si puedes Visualizar esto es porque tienes permisos Privado con JWT.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String amdin() {
+    return "Si puedes Visualizar esto es porque tienes permisos Privado con JWT  y ADMIN ROLE.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")   
+    public String userRole() {
+    return "Si puedes Visualizar esto es porque tienes permisos Privado con JWT  y USER ROLE.";
+    }
+
 }
--- src/main/java/sga/sgajwt/Security/SecurityConfig.java
+++ src/main/java/sga/sgajwt/Security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,7 @@ import sga.sgajwt.JWT.JwtUtils;
 import sga.sgajwt.Services.UserDetailsServiceImpl;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
