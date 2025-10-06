82f5176123cdcff42f2646b7698da96697aaa8fa
willsonp <willvalentinpf@gmail.com>
2025-10-06 08:51:37 -0400

    Confgure Cors in SecutiryConfig

--- src/main/java/sga/sgajwt/Auth/Security/SecurityConfig.java
+++ src/main/java/sga/sgajwt/Auth/Security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package sga.sgajwt.Auth.Security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import sga.sgajwt.Auth.JWT.JwtAuthFilter;
 import sga.sgajwt.Auth.JWT.JwtAutorizationFilter;
@@ -78,6 +83,22 @@ public class SecurityConfig {
             .build();
 
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+
+    }
+
+    
    
 
 }
