62ebe1532911505ef535e64422e524b5ccb5dc32
willsonp <44383690+willsonp@users.noreply.github.com>
2025-11-01 11:09:05 -0400

    upgrade Spring to 3.5.7

--- src/main/java/sga/sgajwt/Auth/Controllers/AuthController.java
+++ src/main/java/sga/sgajwt/Auth/Controllers/AuthController.java
@@ -11,13 +11,12 @@ import sga.sgajwt.Auth.Services.AuthService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("api/v1/auth")
-@CrossOrigin(origins = {"http://localhost:4200"}) 
+// @CrossOrigin(origins = {"http://localhost:4200"}) 
 @RequiredArgsConstructor
 class AuthController {
 
